b9ee5d2dae9e4c85b6e9918fc5cd2a90c17e8e49
Chileshe355 <chileshe355@gmail.com>
2022-04-13 13:02:15 +0200

    creat and test calculator and operator functions

---
+++ script.js
@@ -0,0 +1,20 @@
+//basic calc oprations
+function multi(a,b){
+    return a*b
+   }
+   
+   function add(a,b){
+    return a+b
+   }
+   
+   function minus(a,b){
+    return a-b
+   }
+   
+   function divide(a,b){
+    return a/b
+   }
+   
+   function operate(func,a,b){
+    return func(a,b)
+   }
